4f4c6df7074b5291ee30a1545bcad9d3f058d092
jul <julien@tayon.net>
2022-08-12 17:12:20 +0200

    adding a database to handle the mess
    structure
        source = source file,
        dest  = expected destination,
        metadata = text field with json.
    
    sqlite is so fucking powerful it does all I need including json request
    
    Doublon will be found with
    
    SELECT m.dest, m.source FROM music m
        JOIN
            ( SELECT
                    count(source), dest, source from music
                GROUP BY dest HAVING count(dest) > 1
            ) n ON m.dest=n.dest order by m.dest;
    The fun will come from checking metadata are best and eventually
    creating a score request in sqlite taking into account json data
    such as quality, encoding, format ...

--- find_mp3.py
+++ find_mp3.py
@@ -1,13 +1,32 @@
 #from mutagen import File as get_tags
 #from mutagen.easyid3 import EasyID3 as get_tags_mp3
 from tinytag import TinyTag as tags
+from operator import itemgetter as get
 import archery
-from os import walk, path
+from os import walk, path, makedirs
 from sys import stderr
+from sanitize_filename import sanitize
 import os
 from sys import argv
 from json import dumps
+import sqlite3
+import re
+ms = re.compile(r'''\s+''')
+clean_string=lambda s: ms.sub(' ', s)
+DB="music.db"
+db_exists= path.exists(DB)
+db = sqlite3.connect(DB)
+dbc = db.cursor()
+if not db_exists:
+    dbc.execute("""
+        CREATE TABLE music(
+            source TEXT PRIMARY KEY NOT NULL,
+            dest TEXT NOT NULL,
+            metadata TEXT NOT NULL)""")
+    db.commit()
 pd = lambda d: print(dumps(d, indent=4) + "\n")
+SANDBOX_DIR=path.join(path.expanduser("~"), "todel_mp3_test")
+
 
 wanted=[
         lambda fn : fn.lower()[-3:] in {"mp3", "mp4"} and open(fn, "rb").read(3) ==b'\x49\x44\x33',
@@ -21,11 +40,13 @@ def is_wanted(file):
         except:
             pass
 
+
+
 def tag_to_dict(tag,**extra):
-    cleaned = { k:v for k,v in tag.as_dict().items() if v }
+    cleaned = { k:v and  v.strip() if hasattr(v, "strip") else v or ""  for k, v in tag.as_dict().items() }
     assert not cleaned.keys() & extra.keys(), "keys in extra conflict"
     for k, v in extra.items():
-        cleaned[k] = v
+        cleaned[k] = v.strip() if hasattr(v, "strip") else v
     return cleaned
 start = "."
 try:
@@ -37,36 +58,59 @@ except:
 if not path.exists(start):
     raise Exception("%s not a valid directory to look for")
 
-with open("music_found.txt", "w", encoding='utf-8') as list_of_music_found:
-    n_found = 0
+with \
+    open("music_found.txt", "w", encoding='utf-8') as list_of_music_found, \
+    open("cant_parse.txt", "w", encoding='utf-8')  as list_of_music_wo_tags, \
+    open("collision.txt", "w", encoding='utf-8')  as list_of_collision:
+    n_found,n_errors, n_ignored, n_collisions = 0, 0, 0, 0
     for root, dirs, names in os.walk(start):
-        fp_to_files = { path.join(root, name): name for name in names } 
+        fp_to_files = { path.join(root, name): sanitize(name) for name in names } 
         files = fp_to_files.keys()
         found = list(filter(is_wanted,files))
         n_found += len(found)
-        if len(found):
-            #print(root)
-            for music in found:
-                try:
+        for music in found:
+            try:
+                res=tag_to_dict(tags.get(music), dir=root, filename=music)
 
-                    res=tag_to_dict(tags.get(music), dir=root, filename=music)
-                    if not res.keys() & { "title", "artist", "track" } or  res.get("duration",1) < 1:
-                        print(f'{music} is fishy')
-                        #pd(res)
-                    else:
-                        list_of_music_found.write(f'{music}\n')
-                        artist = res.get("artist", "unknown").title()
-                        album = res.get("album", "unknown").title()
-                        name = fp_to_files[music]
-                        print(f'{name} -> \n   {artist}/{album}/{name}')
-                except:
-                    from pdb import set_trace; set_trace()
-                    stderr.write(f"failed reading {music}\n")
-    print(f"found {n_found} items")
+                if res.get("duration",1) < 1:
+                    pd(res)
+                    print(f'{music} is fishy not added to music')
+                    n_ignored+=1
+                else:
+                    list_of_music_found.write(f'{music}\n')
+                    album = clean_string(res.get("album", "unknown").title())
+                    artist = clean_string(res.get("artist", "unknown").title())
+                    name = fp_to_files[music]
+                    dest_dir = path.join(SANDBOX_DIR, sanitize(artist), sanitize(album))
+                    dest = path.join(dest_dir, sanitize(clean_string(name)))
+                    dbc.execute("INSERT INTO music VALUES (?, ?, ?)", (music, dest, dumps(res, indent=4)))
+                    try:
+                        makedirs(dest_dir, exist_ok=True)
+                        if path.exists(dest):
+                            print(f"collision on {dest}")
+                            n_collisions+=1
+                            list_of_collision.write(f"{name} -> {dest}\n")
+                        else:
+                            with open(dest, "a") as f:
+                                print(f'creating {name} -> \n   {dest}')
+                    except Exception as e:
+                        print(e)
+                        pd(res)
+                        from pdb import set_trace; set_trace()
+                        n_errors+=1
+                        stderr.write(f"failed wrintg {music} to {dest}\n")
+            except Exception as e:
+                print(e)
+                list_of_music_wo_tags.write(f"{music}\n")
+                from pdb import set_trace; set_trace()
+                n_errors+=1
+                print(f"couldn't parse {music}")
+    print(f"found {n_found} items, {n_errors} errors seen, {n_ignored} ignored, {n_collisions} collisions")
 
         #print(sum(path.getsize(path.join(root, name)) for name in files if name.endswith("mp3")),
                 #end=" ")
 
+db.commit()
 
 
 
