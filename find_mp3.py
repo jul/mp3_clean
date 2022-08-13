b1735058a61237bd7cb1ddbb17dbf5282cc2652b
jul <julien@tayon.net>
2022-08-13 02:15:18 +0200

    it seems JSON in db is overkill and I can use db as a huge CSV on steroid since out of 6033 sample keys are always the same. Noice.
    However duration is not a reliable metadata. Music files tagged as less than
    1 seconds actually plays ... for 5 minutes.
    
    Added recovery. And so far I do not hit the set_trace... I will have to
    make a hugerer sample.

--- find_mp3.py
+++ find_mp3.py
@@ -2,7 +2,7 @@
 #from mutagen.easyid3 import EasyID3 as get_tags_mp3
 from tinytag import TinyTag as tags
 from operator import itemgetter as get
-import archery
+from archery import mdict
 from os import walk, path, makedirs
 from sys import stderr
 from sanitize_filename import sanitize
@@ -14,105 +14,101 @@ import re
 ms = re.compile(r'''\s+''')
 clean_string=lambda s: ms.sub(' ', s)
 DB="music.db"
+os.unlink(DB)
 db_exists= path.exists(DB)
 db = sqlite3.connect(DB)
 dbc = db.cursor()
-if not db_exists:
-    dbc.execute("""
-        CREATE TABLE music(
-            source TEXT PRIMARY KEY NOT NULL,
-            dest TEXT NOT NULL,
-            metadata TEXT NOT NULL)""")
-    db.commit()
+dbc.execute("""
+    CREATE TABLE music(
+        source TEXT PRIMARY KEY NOT NULL,
+        dest TEXT NOT NULL,
+        metadata TEXT,
+        filename TEXT NOT NULL,
+        sdir TEXT NOT NULL,
+        artist TEXT,
+        album TEXT,
+        title TEXT,
+        duration FLOAT,
+        bitrate FLOAT,
+        extension TEXT
+        )""")
+db.commit()
 pd = lambda d: print(dumps(d, indent=4) + "\n")
 SANDBOX_DIR=path.join(path.expanduser("~"), "todel_mp3_test")
 
 
-wanted=[
-        lambda fn : fn.lower()[-3:] in {"mp3", "mp4"} and open(fn, "rb").read(3) ==b'\x49\x44\x33',
-        lambda fn : fn.lower()[-4:] in {"flac", ".wma", ".aac", ".wav"},
-        ]
-def is_wanted(file):
-    for test in wanted:
-        try:
-            if test(file):
-                return True
-        except:
-            pass
+def is_wanted(filename):
+    return filename.lower()[-4:] in { ".mp3", "flac",".ogg", ".wma", ".aac", ".wav"}
 
-
-
-def tag_to_dict(tag,**extra):
-    cleaned = { k:v and  v.strip() if hasattr(v, "strip") else v or ""  for k, v in tag.as_dict().items() }
-    assert not cleaned.keys() & extra.keys(), "keys in extra conflict"
-    for k, v in extra.items():
-        cleaned[k] = v.strip() if hasattr(v, "strip") else v
-    return cleaned
 start = "."
 try:
-    print(argv)
     start = argv[1]
-    print(start)
 except:
     pass
+
 if not path.exists(start):
     raise Exception("%s not a valid directory to look for")
 
-with \
-    open("music_found.txt", "w", encoding='utf-8') as list_of_music_found, \
-    open("cant_parse.txt", "w", encoding='utf-8')  as list_of_music_wo_tags, \
-    open("collision.txt", "w", encoding='utf-8')  as list_of_collision:
+score = mdict(keys=mdict(), artist=mdict())
+with open("cant_parse.txt", "w", encoding='utf-8')  as list_of_music_wo_tags:
     n_found,n_errors, n_ignored, n_collisions = 0, 0, 0, 0
     for root, dirs, names in os.walk(start):
-        fp_to_files = { path.join(root, name): sanitize(name) for name in names } 
-        files = fp_to_files.keys()
+        fp_to_fn = { path.join(root, name): sanitize(name) for name in names } 
+        files = fp_to_fn.keys()
         found = list(filter(is_wanted,files))
         n_found += len(found)
         for music in found:
             try:
-                res=tag_to_dict(tags.get(music), dir=root, filename=music)
+                res=mdict({k:v and v.strip() if hasattr(v, "strip") else v for k, v in tags.get(music).as_dict().items()})
+                score["keys"] += mdict({ k : 1 for k in res.keys()})
+                res+=mdict(_dir=root, _src=music, _fname=fp_to_fn[music])
+                extension = fp_to_fn[music].split(".")[-1]
+                album = clean_string((res.get("album", "unknown") or "").title()) or "unkown"
+                res["album"] = album
+                artist = clean_string((res.get("artist", "unknown") or "").title()) or "unknown"
+                res["artist"] = artist
+                dest_dir = path.join(SANDBOX_DIR, sanitize(artist), sanitize(album))
+                dest = path.join(dest_dir, sanitize(clean_string(fp_to_fn[music])))
 
-                if res.get("duration",1) < 1:
-                    pd(res)
-                    print(f'{music} is fishy not added to music')
-                    n_ignored+=1
-                else:
-                    list_of_music_found.write(f'{music}\n')
-                    album = clean_string(res.get("album", "unknown").title())
-                    artist = clean_string(res.get("artist", "unknown").title())
-                    name = fp_to_files[music]
-                    dest_dir = path.join(SANDBOX_DIR, sanitize(artist), sanitize(album))
-                    dest = path.join(dest_dir, sanitize(clean_string(name)))
-                    dbc.execute("INSERT INTO music VALUES (?, ?, ?)", (music, dest, dumps(res, indent=4)))
-                    try:
-                        makedirs(dest_dir, exist_ok=True)
-                        if path.exists(dest):
-                            print(f"collision on {dest}")
-                            n_collisions+=1
-                            list_of_collision.write(f"{name} -> {dest}\n")
+                if artist == "unknown":
+                    if "-" in (res.get("title","") or ""):
+                        artist = (res.get("title") or "").split('-')[0].strip()
+                        print(f"r <{res['title']}> give artist <{artist}>")
+                    elif "-" in res["_fname"]:
+                        r = res["_fname"].split("-")[-2].strip()
+                        if not r.isdigit() or r == "311":
+                            artist = r
+                            print(f"r <{res['_fname']}> give artist <{artist}>")
                         else:
-                            with open(dest, "a") as f:
-                                print(f'creating {name} -> \n   {dest}')
-                    except Exception as e:
-                        print(e)
-                        pd(res)
-                        from pdb import set_trace; set_trace()
-                        n_errors+=1
-                        stderr.write(f"failed wrintg {music} to {dest}\n")
+                            print(f'artist is not in {res["_fname"]}')
+
+                score["artist"] += mdict({res.get("artist","unknown"):1})
+                dbc.execute(
+                    "INSERT INTO music VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (music, dest, dumps(res, indent=4)) + \
+                    tuple(map(
+                        res.get,
+                        ("_fname", "_dir", "artist", "album", "title", "duration", "bitrate", "extension" )))
+                )
+                makedirs(dest_dir, exist_ok=True)
+                if path.exists(dest):
+                    print(f"c", end="")
+                    n_collisions+=1
+                else:
+                    with open(dest, "a") as f:
+                        print(".", end="")
             except Exception as e:
                 print(e)
-                list_of_music_wo_tags.write(f"{music}\n")
                 from pdb import set_trace; set_trace()
+                list_of_music_wo_tags.write(f"{music}\n")
                 n_errors+=1
                 print(f"couldn't parse {music}")
     print(f"found {n_found} items, {n_errors} errors seen, {n_ignored} ignored, {n_collisions} collisions")
-
-        #print(sum(path.getsize(path.join(root, name)) for name in files if name.endswith("mp3")),
-                #end=" ")
-
 db.commit()
 
-
-
+print("keys seens for all metadata")
+pd(score["keys"])
+print("artist most seen")
+pd(dict(sorted(score["artist"].items(), key=get(1), reverse=True)[:30]))
 
 
